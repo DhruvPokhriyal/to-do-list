b4f889f204f047b91465ef288d68108d77c0501b
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-12-13 14:18:17 +0530

    Add the listName validation and close button functionality to the form

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const myTask = [];
 const importantTask = [];
 const tasks = [];
 
+const all_list = { myTask, importantTask, tasks };
+
 const isImportant = false;
 
 class Task {
@@ -17,9 +19,29 @@ class Task {
 
 const lists = document.querySelector(".lists");
 const modal = document.querySelector(".modal");
-modal.showModal();
 
 const newListButton = document.querySelector(".new-list");
+newListButton.addEventListener("click", () => {
+    modal.showModal();
+    const modalForm = modal.querySelector("form");
+    const closeButton = modalForm.querySelector(".close");
+    closeButton.addEventListener("click", () => {
+        modalForm.reset();
+        modal.close();
+    });
+    modalForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const listName = modalForm.querySelector("input").value;
+        for (const key in all_list) {
+            if (listName == key) {
+                alert("List name already exist.");
+                modalForm.reset();
+            } else {
+                all_list[listName] = [];
+            }
+        }
+    });
+});
 
 const taskForm = document.querySelector("#taskForm");
 
